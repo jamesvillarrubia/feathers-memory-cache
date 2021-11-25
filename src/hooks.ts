93436dbd200ca0d0b71a554521972e0e72f91d17
James Villarrubia <james.villarrubia@gmail.com>
2021-11-24 20:18:38 -0500

    feat: yes the addition is made

--- src/hooks.ts
+++ src/hooks.ts
@@ -17,6 +17,8 @@ import { setup } from './setup'
 import { purgeId, CustomHash, CustomKey } from './utils';
 import { HookContext } from '@feathersjs/feathers';
 
+
+
 export interface Options {
   scope?: string;
   customHash?: CustomHash;
